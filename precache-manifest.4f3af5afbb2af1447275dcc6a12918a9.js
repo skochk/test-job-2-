6d712463c436339c7c4035b2b8e72e69143b9c71
skochk <kochkares@gmail.com>
2020-07-28 14:05:09 +0300

    Updates

---
+++ precache-manifest.4f3af5afbb2af1447275dcc6a12918a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef30702a43551bb4571c424925f4d6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a5217f1119767452fb9",
+    "url": "/static/css/main.08441e14.chunk.css"
+  },
+  {
+    "revision": "f5fe20046240e5164650",
+    "url": "/static/js/2.eed4a8b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eed4a8b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5217f1119767452fb9",
+    "url": "/static/js/main.cc483a5a.chunk.js"
+  },
+  {
+    "revision": "112f4824778d80c16c47",
+    "url": "/static/js/runtime-main.c2288638.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
